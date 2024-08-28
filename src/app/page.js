aa821395fc60a51c5c473e91cbb470ba9f7d4eaa
Umer Farooq <umer.farooq@dicetech.dev>
2024-08-28 11:39:26 +0500

    Update app version to 0.1.1

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import AllItems from "./all-items/page";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.welcome}>Welcome</h1>
+      <h1 className={styles.welcome}>Welcome to let's shop</h1>
       <h4 className={styles.note}>Here u can find all the products you want</h4>
       <AllItems></AllItems>
     </div>
